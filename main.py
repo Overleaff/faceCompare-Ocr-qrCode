a0d82411f4551388f96cbed96d3ef425770c951a
Overleaff <x.kirigayakazuto16.5@gmail.com>
2022-08-25 11:50:56 +0700

    wechat qr code model

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import cv2
 from display import *
 import matplotlib.pyplot as plt
 
-path = "/home/kiet/Documents/faceReg+ocr/samples/qrC.png"
+path = "/home/kiet/Documents/faceReg+ocr/samples/21.jpg"
 
 im = cv2.imread(path)
 grayy = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-detector = cv2.wechat_qrcode_WeChatQRCode("wechat/detect.prototxt", "wechat/detect.caffemodel", "wechat/sr.prototxt", "sr.caffemodel")
-data, bbox, _ = detector.detectAndDecode(grayy)
+detector = cv2.wechat_qrcode_WeChatQRCode(  "wechat/detect.prototxt", 
+                                            "wechat/detect.caffemodel", 
+                                            "wechat/sr.prototxt", 
+                                            "wechat/sr.caffemodel")
+
+data, bbox = detector.detectAndDecode(grayy)
 
 print(data)
 print(bbox)
 
-box = displayBbox(grayy, bbox)
-
+#box = displayBbox(grayy, bbox)
+'''
 plt.imshow(box, cmap='gray')
 plt.show()
-
+'''
 
